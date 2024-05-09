3647d00da0bb8ddb92008abd78c33709f8a731dd
peggyp <proffit.peggy@yahoo.fr>
2024-05-09 12:49:49 +0200

    Adding ViewTransactions path

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Sign_in from './pages/Sign_In';
+import ViewTransactions from './pages/ViewTransactions';
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer';
 
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/sign-in" element={<Sign_in/>}/>
+          <Route path="/view-transactions" element={<ViewTransactions/>}/>
         </Routes>
       <Footer/>
       </BrowserRouter>
