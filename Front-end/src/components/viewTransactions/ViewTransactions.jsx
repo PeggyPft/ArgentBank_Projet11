c6fed25e874313c88f86c5235c9619141eb1d2f1
peggyp <proffit.peggy@yahoo.fr>
2024-05-13 11:29:56 +0200

    Adding text

--- Front-end/src/components/viewTransactions/ViewTransactions.jsx
+++ Front-end/src/components/viewTransactions/ViewTransactions.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const ViewTransactions = () => {
+const ViewTransactions = ({title, balance, availableBalance}) => {
     return (
         <div>
             <div className='container_viewTransactions'>
                 <section className="account">
                     <div className="account-content-wrapper">
-                        <h3 className="account-title"></h3>
-                        <p className="account-amount"></p>
-                        <p className="account-amount-description"></p>
+                        <h3 className="account-title">{title}</h3>
+                        <p className="account-amount">{balance}</p>
+                        <p className="account-amount-description">{availableBalance}</p>
                     </div>     
                     <div className="account-content-wrapper cta">
                         <button className="transaction-button">View transactions</button>
