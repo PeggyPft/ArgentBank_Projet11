cbb6d3889b64cfbef02422e5188e928007caa1ca
peggyp <proffit.peggy@yahoo.fr>
2024-05-23 14:46:44 +0200

    Update for style in "editUserName" page

--- Front-end/src/components/footer/Footer.jsx
+++ Front-end/src/components/footer/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Footer = () => {
+    const location = useLocation();
+    const isEditUserNamePage = location.pathname === '/editUserName';
+
     return (
-        <div className='container_footer'>
+        <div className={`container_footer ${isEditUserNamePage ? 'no-border' : ''}`}>
             <p className="footer-text">Copyright 2020 Argent Bank</p>
         </div>
     );
