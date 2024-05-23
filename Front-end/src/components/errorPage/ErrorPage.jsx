f7f62bb62176a75334f2c98b50277c6e769fe7ca
peggyp <proffit.peggy@yahoo.fr>
2024-05-23 14:51:52 +0200

    Added distinction between "Error 401" and "Error 404"

--- Front-end/src/components/errorPage/ErrorPage.jsx
+++ Front-end/src/components/errorPage/ErrorPage.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Header from '../header/Header';
 
 const ErrorPage = () => {
+    const location = useLocation();
+    const {pathname} = location;
+
+    let errorCode = '';
+    let errorAlert = '';
+    let errorMessage = '';
+
+    if(pathname === '/profile' || pathname === '/editUserName') {
+        errorCode = '401';
+        errorAlert = 'authentification requise';
+        errorMessage = 'Vous devez vous connecter pour accéder à cette page';
+    } else {
+        errorCode = '404';
+        errorAlert = 'Désolé, page introuvable';
+        errorMessage = 'La page que vous avez demandée est introuvable';
+    }
+    
     return (
         <div>
             <Header/>
             <div className='errorPage'>
-                <h1>404</h1>
-                <p>Merci de bien vouloir vous connecter.</p>
+                <h1>{errorCode}</h1>
+                <p>{errorAlert}</p>
+                <p>{errorMessage} </p>
 
                 <div className="errorLink">
                     <Link to="/sign-in">Retourner sur la page de connexion</Link>
